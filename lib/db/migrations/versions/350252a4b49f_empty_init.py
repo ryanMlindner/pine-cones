6be4f80bef71950afac096c10b69a923c9a1f9d9
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-07-15 16:59:39 -0600

    alembic versioning

---
+++ lib/db/migrations/versions/350252a4b49f_empty_init.py
@@ -0,0 +1,24 @@
+"""empty init
+
+Revision ID: 350252a4b49f
+Revises: 
+Create Date: 2023-07-15 16:59:17.866437
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '350252a4b49f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
